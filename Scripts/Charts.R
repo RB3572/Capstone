dc4dd917e70d8b8fa9adb4fc94965f96dd64caab
RB3572 <86612265+RB3572@users.noreply.github.com>
2021-07-07 13:19:39 -0500

    Add files via upload

---
+++ Scripts/Charts.R
@@ -0,0 +1,22 @@
+install.packages("quantmod")
+library(quantmod)
+
+#getting data
+tickers = "AMZN"
+
+getSymbols(tickers)
+head(AMZN)
+dim(AMZN)
+AMZN[3651,1]
+s = 1
+x = 3650
+plot(AMZN[s:(s+x), 1], type = "l", col = 7, pch="o",
+     main = "AMZN Opening, High, Low, Closing, and Adjusted Prices",
+     ylab = " Price",
+     xlab = "Date", 
+     lty=1)
+lines(AMZN[s:(s+x), 2], type = "l", col = 2, lty=1)
+lines(AMZN[s:(s+x), 3], type = "l", col = 3, lty=1)
+lines(AMZN[s:(s+x), 4], type = "l", col = 4, lty=1)
+lines(AMZN[s:(s+x), 5], type = "l", col = 5, lty=1)
+lines(AMZN[s:(s+x), 6], type = "l", col = 6, lty=1)
