899d578f488ee9d51eb5324f73c885e8c8bd554d
Ferry <alessandro.ferrari.2004@gmail.com>
2021-07-09 17:09:03 -0400

    cleaned up code and renamed file to comply with standard

--- Scripts/src/detect-cross.R
+++ Scripts/src/detect-cross.R
@@ -1,3 +1,8 @@
+source("src/get-correct-data.R")
+
+tickerName <- c("AAPL")
+
+# TODO: Softcode all the indexing
 detectCross <- function(data){
     
     is50Higher = FALSE
@@ -35,32 +40,7 @@ detectCross <- function(data){
     }
 return (crosses)
 }
-getCorrectData = function(stockArray) {
-    library(quantmod)
-    library(TTR)
-    library(ezstocks)
-    
-    # Get data
-    stockData <- getStockData(stocks = stockArray, startYear = "2015", startMonth = "01", startDay = "01")
-    
-    # Manipulate data
-    sortedStockData <- cbind(getHighData(stockData), getOpenData(stockData), getCloseData(stockData), getLowData(stockData), getVolume(stockData), getAdjustedData(stockData))
-    
-    for (i in 1:(length(stockArray)*6)) {
-        sortedStockData <- cbind(sortedStockData, SMA(sortedStockData[, i], 50))
-        
-        colnames(sortedStockData)[length(stockArray) * 6 + i] <- paste0(colnames(sortedStockData)[i], ".SMA50")
-    }
-    
-    for (i in 1:(length(stockArray)*6)) {
-        sortedStockData <- cbind(sortedStockData, SMA(sortedStockData[, i], 200))
-        
-        colnames(sortedStockData)[((length(stockArray) * 6) * 2) + i] <- paste0(colnames(sortedStockData)[i], ".SMA200")
-    }
-    
-    return(sortedStockData)
-}
 
-data = getCorrectData(c("AAPL"))
+data = getCorrectData(tickerName)
 c1 = detectCross(data)
 data = cbind(data, c1)
