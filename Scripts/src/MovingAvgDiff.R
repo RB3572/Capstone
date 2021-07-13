574669e74204c86fc16e598542e485e01906a459
Rishi Bhargava <rishibhargava@rishis-mbp.lan>
2021-07-13 13:51:35 -0500

    added function MovingAvgDiff()

---
+++ Scripts/src/MovingAvgDiff.R
@@ -0,0 +1,55 @@
+#source("src/find-index-attribute-ticker.R")
+source("/Users/rishibhargava/Desktop/Capstone/Scripts/src/get-correct-data.R")
+#source("src/find-index-attribute-ticker.R")
+source("/Users/rishibhargava/Desktop/Capstone/Scripts/src/find-index-attribute-ticker.R")
+tickerName <- c("AAPL")
+
+MovingAvgDiff <- function(sma50, sma200){
+    
+    is50Higher = FALSE
+    crosses = c()
+    priceDiffs = c()
+    for(i in 1:(length(sma50) - 1)){
+        
+        if(is.na(sma50[i]) || is.na(sma200[i])){
+            crosses = append(crosses, NA)
+            priceDiffs = append(priceDiffs, sma200[i]-sma50[i])
+            
+        } 
+        else{
+            if(is50Higher){
+                if(sma50[i] < sma200[i]){
+                    is50Higher = FALSE
+                    #death cross
+                    crosses = append(crosses, 'd')
+                    print(paste0("death cross at ", i))
+                    priceDiffs = append(priceDiffs, 0)
+                }
+                else{
+                    crosses = append(crosses, NA)
+                    priceDiffs = append(priceDiffs, sma200[i]-sma50[i])
+                    
+                }
+            }
+            
+            if(is50Higher == FALSE){
+                if(sma50[i] > sma200[i]){
+                    is50Higher = TRUE
+                    #golden cross
+                    crosses = append(crosses,'g')
+                    print(paste0("golden cross at ", i))
+                    priceDiffs = append(priceDiffs, 0)
+                }
+                else{
+                    crosses= append(crosses, NA)
+                    priceDiffs = append(priceDiffs, sma200[i]-sma50[i])
+                }
+            }
+        }
+    }
+    return (priceDiffs)
+}
+
+data = getCorrectData(tickerName)
+crosses = MovingAvgDiff(data$sortedStockData$AAPL.Close.SMA50, data$sortedStockData$AAPL.Close.SMA200)
+newdata = cbind(data$sortedStockData, crosses)
