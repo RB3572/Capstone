6b8d06ef395ae00a515075e2e46d22f18516fecd
Rishi Bhargava <rishibhargava@rishis-mbp.lan>
2021-07-14 09:21:26 -0500

    Shiny edit

--- CapstoneShiny/server.R
+++ CapstoneShiny/server.R
@@ -2,6 +2,7 @@ server <- function(input, output) {
     output$plot <- renderPlot({
         source("../Scripts/src/keras-nn-regressor.R")
         keras-nn-regressor(ticker = input$ticker, epochs = input$epochs)
+        #modelFunction(ticker = input$ticker, epochs = input$epochs)
     })
     
 }
